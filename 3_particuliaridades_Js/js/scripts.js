9e72ea8b225538d630b9d5e537d046ab79607c3c
AndrewPorto <andrewluizporto@hotmail.com>
2022-07-05 16:36:28 -0300

    ex 3 concluido

---
+++ 3_particuliaridades_Js/js/scripts.js
@@ -0,0 +1,22 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+// Declara uma variável
+
+var a = 1;
+
+a = 'teste';
+
+console.log(a)
+
+// Este é um comentári
+
+/* Este
+    é 
+    um
+    comentário
+    de
+    multiplas
+    linas
+*/
