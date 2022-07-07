ee54c67822bace4456928220c18db3afde8a3179
AndrewPorto <andrewluizporto@hotmail.com>
2022-07-07 17:37:19 -0300

    Ex 6, 7, 8, 9 adicionados

---
+++ 6_strings/js/scripts.js
@@ -0,0 +1,18 @@
+var nome = "Andrew";
+var sobrenome = "Porto";
+
+var nomeCompleto = nome + " " + sobrenome
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(nomeCompleto);
+
+var frase = 'Está é uma frase complexa';
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase)
+
+document.write('Ele disse: "Olá" ')
---
+++ 8_nul_undefined/js/scripts.js
@@ -0,0 +1,13 @@
+//Hoisting - içamento
+console.log(sobrenome);
+
+
+var nome = null;
+var sobrenome = "Porto";
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "Andrew";
+
+console.log(nome);
