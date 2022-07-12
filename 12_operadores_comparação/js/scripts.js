1df2da40301ae21dc744876c953632126142ffdc
AndrewPorto <andrewluizporto@hotmail.com>
2022-07-12 15:41:31 -0300

    ex12 concluido

---
+++ 12_operadores_comparação/js/scripts.js
@@ -0,0 +1,32 @@
+var idade = 18;
+var possuiCarro = 1;
+
+if (idade >= 18) {
+    console.log("O usuario pode fazer a carteira");
+}
+
+if (idade <= 17) {
+    console.log("O usuário não pode fazer a carteira");
+}
+
+if (possuiCarro) {
+    console.log("O usuário já pode andar de carro");
+}
+
+var nome = "Andrew";
+
+if (nome == "Matheus") {
+    console.log("O seu nome é Matheus");
+}
+
+if (nome != "Matheus") {
+    console.log("O seu nome não é Andrew");
+}
+
+if (20 > 100) {
+    console.log("Passou");
+}
+
+if (100 < 20) {
+    console.log("Passou 2");
+}
